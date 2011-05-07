94bdbe58d5406e4e01e4ceb4e6d40463e0209a8b
Mark O'Shea <igus.mail@gmail.com>
2011-05-07 19:21:29 +0100

    adding user login authentication with authlogic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRoutes::Application.config.secret_token = '9b2f632871280bf6fbccec01ad1cd7cb289e516e09a01706c4b72503b9571a9fd5a8275f258b2e1e864ed9c21b086550d792c0755b6afc61685410c259a461e4'
