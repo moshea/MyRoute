f1d9f29506f1e7a9fdb0d8586e4dcee929bbadc1
Mark O'Shea <igus.mail@gmail.com>
2011-06-02 14:25:41 +0100

    [#2 status:resolved]

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,8 +3,8 @@ class Route < ActiveRecord::Base
 	validates_presence_of :name, {:message => "A name is needed!"}
 	validates_presence_of :gmap_coords
 	
-	def static_map
-		GoogleMap.new(self).static_map_url
+	def static_map(params={})
+		GoogleMap.new(self).static_map_url(params)
 	end
 	
 end
