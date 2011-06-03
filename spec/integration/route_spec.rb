c10e438ed9cc6244d6fdeff405ceec99b8a26e13
Mark O'Shea <igus.mail@gmail.com>
2011-06-03 19:31:41 +0100

    fixing an integration test

--- spec/integration/route_spec.rb
+++ spec/integration/route_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe "the new route page", :type => :request do
 	
+	before(:each) do
+		@user = Factory.build(:user)
+	end
+	
 	it "renders the new route page when I visit new_route_path" do
 		visit root_path
-		#add sign up as part of test
+		click_link "Register"
+		fill_in "user_username", :with => @user.username
+		fill_in "user_email", :with => @user.email
+		fill_in "user_password", :with => @user.password
+		fill_in "user_password_confirmation", :with => @user.password
+		click_button "Create User"
+
+		visit new_route_path
 		page.should have_selector('input#route_name')
 		page.should have_selector('textarea#route_description')
 	end
